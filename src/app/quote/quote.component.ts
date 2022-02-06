e127dfbe9e17812d108b7e06712051aeb0ae3af6
Juliet-Ol <agutjuliet@gmail.com>
2022-02-06 21:29:10 +0300

    create quote component  and import quote

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  
+  quotes: Quote[]= [
+    {id:1, name: "Nelson Mandela", description:"The gretest glory in living lies not in never falling, but in rising every time we fall."},
+    {id:2, name:"Steve Jobs", description:"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma-which is living with the rewults of other people's thinking." },
+    {id:2, name:"Mother Theresa", description:"Spread love everywhere you go. Let no one ever come to you without leaving happier." },
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
