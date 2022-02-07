8dea4bdf7d67314395cac2e95d330360046fc017
Juliet-Ol <agutjuliet@gmail.com>
2022-02-07 21:43:53 +0300

    fit the background picture to the screen

---
+++ src/app/like.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'like-box',
+  templateUrl: 'like.component.html',
+  
+})
+export class likeComponent{
+  numberOfLikes : number | undefined;
+
+}
