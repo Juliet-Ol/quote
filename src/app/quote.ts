27e722bf2addb833441a82bc5aae53d77a114412
Juliet-Ol <agutjuliet@gmail.com>
2022-02-07 13:55:25 +0300

    link the form to the main application

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription: boolean;
-    constructor(public name: string,public description:string,public author:string) {
+    constructor(public name: string,public description:string,public author:string,public date:Date) {
         this.showDescription=false;
     }
     
